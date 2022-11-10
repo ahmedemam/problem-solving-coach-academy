e0f14cb3c99595a0f4ae4a24884e5e90c2d66309
ahmed.emam <ahmed.emam@cremeglobal.com>
2022-11-10 11:29:38 +0200

    solving sheet 3

--- sheet3-Built-in-functions-strings/C-Gravity-Flip.cpp
+++ sheet3-Built-in-functions-strings/C-Gravity-Flip.cpp
@@ -6,4 +6,17 @@ using namespace std;
 
 int main()
 {
+  int cubes;
+  cin >> cubes;
+  int cube[cubes];
+  for (int i = 0; i < cubes; i++)
+  {
+    cin >> cube[i];
+  }
+
+  sort(cube, cube + cubes);
+  for(int i=0; i<cubes; i++){
+    cout << cube[i] << " ";
+  }
+  cout << endl;
 }
